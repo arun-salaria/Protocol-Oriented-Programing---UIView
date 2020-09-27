5baa89d1e46f2fba20dbb661788b0f997dd01dc3
arun-salaria <arun.s.salaria@gmail.com>
2020-09-27 16:14:30 +0530

    POP Example done without structuring and commenting

--- Protocol_Oriented_Programing---UIView/Protocol_Oriented_Programing---UIView/ViewController.swift
+++ Protocol_Oriented_Programing---UIView/Protocol_Oriented_Programing---UIView/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
+class CustomView:UIView,RoundCorners{
+    
+}
+class CustomLabel:UILabel,RoundCorners,AnimationProtocols,AddShadow{
+    
+}
+class CustomButton:UIButton,RoundCorners{
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animateTextBtn: CustomButton!
+    @IBOutlet weak var samplelbl: CustomLabel!
+    @IBOutlet weak var labelBGView: CustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelBGView.roundCorner(radius: 30)
+        animateTextBtn.roundCorner(radius: 4)
+        
+        samplelbl.roundCorner(radius: 4)
+        samplelbl.addShadow()
+        
     }
 
-
+    @IBAction func didTapOnAnimateBtn(_ sender: Any) {
+        samplelbl.fadeAnimation()
+    }
 }
 
---
+++ Protocol_Oriented_Programing---UIView/Protocol_Oriented_Programing---UIView/ViewProtocols.swift
@@ -0,0 +1,35 @@
+//
+//  ViewProtocols.swift
+//  Protocol_Oriented_Programing---UIView
+//
+//  Created by arun on 27/09/20.
+//
+import UIKit
+
+protocol RoundCorners {}
+extension RoundCorners where Self: UIView {
+    func roundCorner(radius:CGFloat) {
+        layer.cornerRadius = radius
+        clipsToBounds = true
+    }
+}
+
+protocol AddShadow {}
+extension AddShadow where Self: UIView {
+    func addShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 10
+    }
+}
+
+protocol AnimationProtocols {}
+extension AnimationProtocols where Self:UIView {
+    func fadeAnimation(){
+        self.alpha = 0.0
+        UIView.animate(withDuration: 1) {
+            self.alpha = 1.0
+        }
+    }
+}
